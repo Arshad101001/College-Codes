7db19f7c51bb7ce2997aec518bff6b75dc8b57f1
Arshad101001 <arshadansari101001@gmail.com>
2026-05-03 00:53:05 +0530

    feat: Practical 4 - Disconnected architechture implemented

---
+++ SEM 2/AWT/Practical 4/DisconnectedDemo/DisconnectedDemo/WebForm1.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DisconnectedDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Employee Form (Disconnected Architecture)</h2>
+            <asp:Label ID="Label1" runat="server" Text="Emp No:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="empNo" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="empName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Address:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="empAddress" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Phone:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="empCont" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="select" runat="server" OnClick="SelectClick" Text="Select" />
+&nbsp;
+            <asp:Button ID="insert" runat="server" OnClick="InsertClick" Text="Insert" />
+&nbsp;
+            <asp:Button ID="update" runat="server" OnClick="UpdateClick" Text="Update" />
+&nbsp;
+            <asp:Button ID="delete" runat="server" OnClick="DeleteClick" Text="Delete" />
+&nbsp;
+            <asp:Button ID="clear" runat="server" OnClick="ClearClick" Text="Clear" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SEM 2/AWT/Practical 4/DisconnectedDemo/DisconnectedDemo/WebForm1.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DisconnectedDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataSet ds;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(
+                    @"Data Source = (LocalDB)\MSSQLLocalDB; 
+                        AttachDbFilename = C:\Users\admin\source\repos\DisconnectedDemo\DisconnectedDemo\App_Data\Database1.mdf;
+                        Integrated Security = True"
+                );
+
+        }
+
+        // SELECT (DISCONNECTED) 
+        protected void SelectClick(object sender, EventArgs e)
+        {
+            con.Open();
+
+            da = new SqlDataAdapter("Select * from employee", con);
+            ds = new DataSet();
+
+            da.Fill(ds);
+
+            con.Close();
+
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+
+        // INSERT
+        protected void InsertClick(object sender, EventArgs e)
+        {
+            Console.WriteLine(empNo.GetType());
+            string qry = "insert into employee values(" + empNo.Text + ", '" + empName.Text + "', '" + empAddress.Text + "', '" + empCont.Text + "')";
+
+            con.Open();
+
+            cmd = new SqlCommand(qry, con);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            Response.Write("Inserted Successfully");
+        }
+
+        // UPDATE
+        protected void UpdateClick(object sender, EventArgs e)
+        {
+            string qry = "update employee set ename = '" + empName.Text + "', eadd = '" + empAddress.Text + "', econt = '" + empCont.Text + "' where eno = " + empNo.Text;
+
+            con.Open();
+
+            cmd = new SqlCommand(qry, con);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            Response.Write("Updated Successfully");
+        }
+
+        // DELETE
+        protected void DeleteClick(object sender, EventArgs e)
+        {
+            string qry = "delete from employee where eno = " + empNo.Text;
+
+            con.Open();
+
+            cmd = new SqlCommand(qry, con);
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            Response.Write("Deleted Successfully");
+        }
+
+        // CLEAR
+        protected void ClearClick(object sender, EventArgs e)
+        {
+            empNo.Text = "";
+            empName.Text = "";
+            empAddress.Text = "";
+            empCont.Text = "";
+        }
+    }
+}
