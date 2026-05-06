27f5a49c4e67ad80d677e7a6047b1aac199a082e
Arshad101001 <arshadansari101001@gmail.com>
2026-05-06 19:43:22 +0530

    feat: Practical 6 - Parameter procedure implemented

---
+++ SEM 2/AWT/Practical 6/ParameterizedProc/ParameterizedProc/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ParameterizedProc.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="ID"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="id" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="name" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Age"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="age" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Country"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="country" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Show Data" OnClick="ShowClick" />
+&nbsp;
+            <asp:Button ID="insert" runat="server" Text="Insert" OnClick="InsertClick" />
+&nbsp;
+            <asp:Button ID="update" runat="server" Text="Update" OnClick="UpdateClick" />
+&nbsp;
+            <asp:Button ID="delete" runat="server" Text="Delete" OnClick="DeleteClick" />
+&nbsp;
+            <asp:Button ID="clear" runat="server" Text="Clear" OnClick="ClearClick" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SEM 2/AWT/Practical 6/ParameterizedProc/ParameterizedProc/WebForm1.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ParameterizedProc
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string str = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                        AttachDbFilename=C:\Users\admin\source\repos\ParameterizedProc\ParameterizedProc\App_Data\Database1.mdf;
+                        Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // SHOW DATA
+        protected void ShowClick(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+
+            SqlDataAdapter da = new SqlDataAdapter("select * from UserRegistration", con);
+
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+
+        // INSERT
+        protected void InsertClick(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+
+            SqlCommand cmd = new SqlCommand("SpMyProcedure", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Action", "insert");
+            cmd.Parameters.AddWithValue("@Name", name.Text);
+            cmd.Parameters.AddWithValue("@Age", age.Text);
+            cmd.Parameters.AddWithValue("@Country", country.Text);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Response.Write("Inserted Successfully");
+        }
+
+        // UPDATE
+        protected void UpdateClick(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+
+            SqlCommand cmd = new SqlCommand("SpMyProcedure", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Action", "update");
+            cmd.Parameters.AddWithValue("@Id", id.Text);
+            cmd.Parameters.AddWithValue("@Name", name.Text);
+            cmd.Parameters.AddWithValue("@Age", age.Text);
+            cmd.Parameters.AddWithValue("@Country", country.Text);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Response.Write("Updated Successfully");
+        }
+
+        protected void DeleteClick(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(str);
+
+            SqlCommand cmd = new SqlCommand("SpMyProcedure", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Action", "delete");
+            cmd.Parameters.AddWithValue("@Id", id.Text);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            Response.Write("deleted Successfully");
+        }
+
+        protected void ClearClick(object sender, EventArgs e)
+        {
+            id.Text = "";
+            name.Text = "";
+            age.Text = "";
+            country.Text = "";
+        }
+    }
+}
