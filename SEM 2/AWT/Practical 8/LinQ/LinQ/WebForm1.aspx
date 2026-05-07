0df708b81a674e3b241302e9aad6ee0651632bfa
Arshad101001 <arshadansari101001@gmail.com>
2026-05-08 00:00:15 +0530

    feat: Practical 8 - LinQ added

---
+++ SEM 2/AWT/Practical 8/LinQ/LinQ/WebForm1.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LinQ.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="ID"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="name" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Age"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="age" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Div"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="div" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label5" runat="server" Text="Dream Job"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="dreamJob" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="insert" runat="server" Text="Insert" OnClick="Insert_Click" />
+&nbsp;
+            <asp:Button ID="update" runat="server" Text="Update" OnClick="Update_Click" />
+&nbsp;
+            <asp:Button ID="delete" runat="server" Text="Delete" OnClick="Delete_Click" />
+&nbsp;
+            <asp:Button ID="select" runat="server" Text="Select" OnClick="Select_Click" />
+            <br />
+            <br />
+
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SEM 2/AWT/Practical 8/LinQ/LinQ/WebForm1.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinQ
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        Database1Entities dc = new Database1Entities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ShowData()
+        {
+            GridView1.DataSource = dc.students.ToList();
+            GridView1.DataBind();
+        }
+
+        // INSERT
+        protected void Insert_Click(object sender, EventArgs e)
+        {
+            student s = new student();
+
+            s.Id = Convert.ToInt32(txtId.Text);
+            s.Name = name.Text;
+            s.Age = Convert.ToInt32(age.Text);
+            s.Div = div.Text;
+            s.DreameJob = dreamJob.Text;
+
+            dc.students.Add(s);
+            dc.SaveChanges();
+
+            Response.Write("Inserted Successfully");
+        }
+
+        // UPDATE
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+
+            student s = dc.students.Single(x => x.Id == id);
+
+            s.Name = name.Text;
+            s.Age = Convert.ToInt32(age.Text);
+            s.Div = div.Text;
+            s.DreameJob = dreamJob.Text;
+
+            dc.SaveChanges();
+
+            ShowData();
+        }
+
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtId.Text);
+
+            student s = dc.students.Single(x => x.Id == id);
+
+            dc.students.Remove(s);
+            dc.SaveChanges();
+
+            ShowData();
+        }
+
+        protected void Select_Click(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+    }
+}
