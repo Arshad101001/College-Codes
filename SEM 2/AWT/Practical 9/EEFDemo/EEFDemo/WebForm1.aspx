3bfb93c3b397525b7b5d04aa33f1b961704df96c
Arshad101001 <arshadansari101001@gmail.com>
2026-05-11 20:28:35 +0530

    feat: Practical 9 - Entity frame work

---
+++ SEM 2/AWT/Practical 9/EEFDemo/EEFDemo/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EEFDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="PID:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="pid" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="name" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Cost:"></asp:Label>
+&nbsp;
+            <asp:TextBox ID="cost" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="insert" runat="server" Text="Insert" OnClick="Insert_Click" />
+&nbsp;
+            <asp:Button ID="update" runat="server" Text="Update" OnClick="Update_Click" />
+&nbsp;
+            <asp:Button ID="delete" runat="server" Text="Delete" OnClick="Delete_Click" />
+&nbsp;
+            <asp:Button ID="show" runat="server" Text="Show Data" OnClick="Show_Click" />
+            <br />
+            <br />
+
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SEM 2/AWT/Practical 9/EEFDemo/EEFDemo/WebForm1.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EEFDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        Database1Entities db = new Database1Entities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ShowData()
+        {
+            GridView1.DataSource = db.products.ToList();
+            GridView1.DataBind();
+        }
+
+        // INSERT
+        protected void Insert_Click(object sender, EventArgs e)
+        {
+            product p = new product();
+
+            p.pid = Convert.ToInt32(pid.Text);
+            p.pname = name.Text;
+            p.pcost = cost.Text;
+
+            db.products.Add(p);
+            db.SaveChanges();
+
+            Response.Write("Inserted Successfully");
+        }
+
+        // UPDATE
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(pid.Text);
+
+            product p = db.products.Single(x => x.pid == id);
+
+            p.pname = name.Text;
+            p.pcost = cost.Text;
+
+            db.SaveChanges();
+
+            ShowData();
+        }
+
+        // DELETE
+        protected void Delete_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(pid.Text);
+
+            product p = db.products.Single(x => x.pid == id);
+
+            db.products.Remove(p);
+            db.SaveChanges();
+
+            ShowData();
+        }
+
+        // SHOW
+        protected void Show_Click(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+    }
+}
