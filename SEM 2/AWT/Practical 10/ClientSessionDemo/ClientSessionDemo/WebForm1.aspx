204f0804d0bd0145bf3f9eaad9c04e341afccf28
Arshad101001 <arshadansari101001@gmail.com>
2026-05-14 01:39:50 +0530

    feat: Practical 10 - Client side state management implemented

---
+++ SEM 2/AWT/Practical 10/ClientSessionDemo/ClientSessionDemo/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ClientSessionDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            User ID: &nbsp;
+            <asp:TextBox ID="userId" runat="server"></asp:TextBox>
+        &nbsp;
+            <asp:Button ID="saveBtn" runat="server" Text="Save in ViewState" OnClick="SaveBtn_Click" />
+        &nbsp;
+            <asp:Button ID="getBtn" runat="server" Text="Get ViewState" OnClick="GetBtn_Click" />
+        &nbsp;
+            <asp:Label ID="viewStateLable" runat="server" ></asp:Label>
+            <br />
+            <br />
+            Hidden Field: &nbsp;
+            <asp:HiddenField ID="HiddenField1" runat="server" Value="Value 123" />
+            <asp:Button ID="hiddenFieldBtn" runat="server" Text="Get Hidden Field" OnClick="HiddenFieldBtn_Click" />
+            <asp:Label ID="hiddenLable" runat="server" ></asp:Label>
+
+            <br />
+            <br />
+            Query String:
+            <asp:TextBox ID="queryStr" runat="server"></asp:TextBox>
+            &nbsp;
+            <asp:Button ID="queryStrBtn" runat="server" Text="Send via QueryString" OnClick="QueryStrBtn_Click" />
+            <br />
+            <br />
+            Cookies:
+            <asp:TextBox ID="cookieTxt" runat="server"></asp:TextBox>
+            <asp:Button ID="createBtn" runat="server" Text="Create Cookie" OnClick="CreateBtn_Click" />
+            &nbsp;
+            <asp:Button ID="readBtn" runat="server" Text="Read Cookie"  OnClick="ReadBtn_Click"/>
+            <asp:Label ID="cookieLabel" runat="server" ></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SEM 2/AWT/Practical 10/ClientSessionDemo/ClientSessionDemo/WebForm1.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClientSessionDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // ViewState Save
+        protected void SaveBtn_Click(object sender, EventArgs e)
+        {
+            ViewState["userId"] = userId.Text;
+            userId.Text = string.Empty;
+
+            Response.Write("ViewState Created Successfully");
+        }
+
+        // ViewState Retrieve
+        protected void GetBtn_Click(object sender, EventArgs e)
+        {
+            if (ViewState["userId"] != null)
+            {
+                viewStateLable.Text = ViewState["userId"].ToString();
+            }
+        }
+
+        // Hidden Field Read
+        protected void HiddenFieldBtn_Click(object sender, EventArgs e)
+        {
+            hiddenLable.Text = HiddenField1.Value;
+        }
+
+        // Query String Send
+        protected void QueryStrBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm2.aspx?data=" + queryStr.Text);
+        }
+
+        // Cookie Create
+        protected void CreateBtn_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["name"].Value = cookieTxt.Text;
+            Response.Cookies["name"].Expires = DateTime.Now.AddMinutes(1);
+
+            cookieLabel.Text = "Cookie Created";
+            cookieTxt.Text = "";
+        }
+
+        // Cookie Read
+        protected void ReadBtn_Click(object sender, EventArgs e)
+        {
+            if (Request.Cookies["name"] == null)
+            {
+                cookieTxt.Text = "No Cookie Found";
+            }
+            else
+            {
+                cookieTxt.Text = Request.Cookies["name"].Value;
+            }
+        }
+    }
+}
